0fe4254e8f71487bd8deaa8386644d359abeca89
Kaiden Kliegl <132409071+kaidenkliegl@users.noreply.github.com>
2024-11-20 23:12:54 -0800

    finished some operations and functionality

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-//create a function that takes any number of arguments
+
+const numbersBtn = document.querySelectorAll('.grid buttom .number')
+
+
+numbersBtn.forEach(element => {
+    numbersBtn.addEventListener('click', function() {
+      console.log('Element clicked!');
+    });
+  });
+
+
 
 //when operator downclicks triggers operations()
 
@@ -7,43 +17,44 @@
 let operator;
 
 
-let operations = (val) => {
-//have your function that test waht the index is 
-let num1 = null;
+// let operations = (val) => {
+// //have your function that test waht the index is 
+// let num1 = null;
 
-let num2 = null;
+// let num2 = null;
 
-if(num1 === null){
-num1 = val
-}else{
-    num2 = val
-}
+// if(num1 === null){
+// num1 = val
+// }else{
+//     num2 = val
+// }
 
-if(num1 !== 0 && num2 !== 0){
-    calculate(num1, operator, num2)
-}
+// if(num1 !== 0 && num2 !== 0){
+//     calculate(num1, operator, num2)
+// }
 
-}
+// }
 
-// calculate function
+// // calculate function
 
-function calculate(num1, operator, num2) {
-    switch (operator) {
-      case "+":
-        return num1 + num2;
-      case "-":
-        return num1 - num2;
-      case "*":
-        return num1 * num2;
-      case "/":
-        if (num2 === 0) {
-          return "Error: Division by zero!";
-        }
-        return num1 / num2;
-      default:
-        return "Invalid operator!";
-    }
-  }
+// function calculate(num1, operator, num2) {
+//     switch (operator) {
+//       case "+":
+//         return num1 + num2;
+//       case "-":
+//         return num1 - num2;
+//       case "*":
+//         return num1 * num2;
+//       case "/":
+//         if (num2 === 0) {
+//           return "Error: Division by zero!";
+//         }
+//         return num1 / num2;
+//       default:
+//         return "Invalid operator!";
+//     }
+//   }
   
 
-  //remember to set for export
+//   //remember to set for export
+// module.exports{}
